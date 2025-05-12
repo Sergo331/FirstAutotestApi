52488f8fd788a3c002e0e5a4cd10b7775317e0ad
Sergo331 <proha331@gmail.com>
2025-05-12 19:22:25 +0300

    Удаление объекта

---
+++ src/test/java/tests/DeleteUserTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeleteUserTest{
+
+    private final String BASE_URL = "https://reqres.in/api/users/2";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        step("Отправка DELETE - запроса");
+        Response response = RestAssured
+                .given()
+                .when()
+                .delete(BASE_URL)
+                .then()
+                .statusCode(204)
+                .extract()
+                .response();
+
+        step("Тело ответа пустое");
+        String responsebody = response.getBody().asString();
+        assertEquals(" ", responsebody, "Тело ответа не является пустым");
+    }
+}
